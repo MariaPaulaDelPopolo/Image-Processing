2c55da5d0a01f74021167313625d515ab768a346
ErikRiel <epmriel@gmail.com>
2023-11-30 21:58:55 +0100

    Background Segmentation

--- BackgroundSegmentation.m
+++ BackgroundSegmentation.m
@@ -1,8 +1,23 @@
 function [outputImage] = BackgroundSegmentation(image)
 
-% CODE HERE...
+maxVal = max(image(:))
+minVal = min(image(:))
+threshold = ((maxVal - minVal) / 20);
+mask_background = image < threshold;
+se = strel('disk', 1);
+mask_background = imclose(mask_background,se);
+se = strel('disk', 12);
+mask_background = imopen(mask_background,se);
 
-outputImage = ...;
+% Smooth the edges using a Gaussian filter
+sigma = 3;
+mask_background = imgaussfilt(double(mask_background), sigma);
+
+% Convert the smoothed image back to a binary image using a threshold
+mask_background = mask_background > 0.5; % Adjust the threshold if needed
+
+
+outputImage = mask_background;
 
 end
 
--- main.m
+++ main.m
@@ -1,33 +1,40 @@
 % Loading the image
-filename = "...";
-extension = ".png"
+view = "COR";
+files = { view + "_1", view + "_2", view + "_3", view + "_4", view + "_5"};
+for i = 1:length(files);
+filename = files{i};
+extension = ".png";
 image= im2double(imread("Input Images/" + filename + extension));
-imshow(image);
+% imshow(image);
 
 % Background segmentation
 bgrSegIm = BackgroundSegmentation(image);
+figure(i);
 imshow(bgrSegIm);
-imwrite(bgrSegIm, "Output Images\" + filename + "_backgroundSegmentation");
+% imwrite(bgrSegIm, "Output Images\" + filename + "_backgroundSegmentation" + extension);
 
 % Skull stripping
-skullStripped = SkullStripFilter(image, bgrSegIm);
+% skullStripped = SkullStripFilter(image, bgrSegIm);
+% figure(i);
+% imshow(skullStripped);
 
-% Skull segmentation
-skuSegIm = SkullSegmentation(skullStripped, bgrSegIm);
-imshow(skuSegIm);
-imwrite(skuSegIm, "Output Images\" + filename + "_SkullSegmentation");
-
-% CSF segmentation
-csfSegIm = CSFSegmentation(image, skullStripped);
-imshow(csfSegIm);
-imwrite(csfSegIm, "Output Images\" + filename + "_CSFSegmentation");
-
-% White matter segmentation
-whmSegIm = WhiteMatterSegmentation(image, csfSegIm, skullStripped);
-imshow(whmSegIm);
-imwrite(whmSegIm, "Output Images\" + filename + "_WhiteMatterSegmentation");
-
-% Grey matter segmentation
-grmSegIm = GreyMatterSegmentation(whmSegIm ,csfSegIm, skullStripped);
-imshow(grmSegIm);
-imwrite(grmSegIm, "Output Images\" + filename + "_GreyMatterSegmentation");
+% % Skull segmentation
+% skuSegIm = SkullSegmentation(skullStripped, bgrSegIm);
+% imshow(skuSegIm);
+% imwrite(skuSegIm, "Output Images\" + filename + "_SkullSegmentation" + extension);
+% 
+% % CSF segmentation
+% csfSegIm = CSFSegmentation(image, skullStripped);
+% imshow(csfSegIm);
+% imwrite(csfSegIm, "Output Images\" + filename + "_CSFSegmentation" + extension);
+% 
+% % White matter segmentation
+% whmSegIm = WhiteMatterSegmentation(image, csfSegIm, skullStripped);
+% imshow(whmSegIm);
+% imwrite(whmSegIm, "Output Images\" + filename + "_WhiteMatterSegmentation" + extension);
+% 
+% % Grey matter segmentation
+% grmSegIm = GreyMatterSegmentation(whmSegIm ,csfSegIm, skullStripped);
+% imshow(grmSegIm);
+% imwrite(grmSegIm, "Output Images\" + filename + "_GreyMatterSegmentation" + extension);
+end;
